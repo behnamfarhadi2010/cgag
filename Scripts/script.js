0f7dedbd6d2d3ec7e14767a22efe5c9405c57891
behnam farhadi <farhadi.behnam@gmail.com>
2025-05-10 08:09:33 -0230

    Fetch image in main page

--- Scripts/script.js
+++ Scripts/script.js
@@ -53,47 +53,4 @@ const imageUrls = [
 
   // fetch the art name from the sample page
   // and add it to the container
-  const infoUrls = [
-    "./samplepage.html",
-    
-  ];
-  
-  const artNameCont = document.getElementById("artNameContainer");
-  
-  infoUrls.forEach(url => {
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        const tempDiv = document.createElement("div");
-        tempDiv.innerHTML = html;
-  
-        const artInfo = tempDiv.querySelector("#artNameContainer");
-  
-        if (artInfo) {
-          const infoDiv = document.createElement("h3");
-          infoDiv.innerHTML = artInfo.innerHTML;
-          infoDiv.style.marginBottom = "100px";
-          infoDiv.style.marginLeft = "110";
-          infoDiv.style.marginTop = "0";
-          infoDiv.style.padding = "10px";
-          infoDiv.style.backgroundColor = "#ffffff";
-          infoDiv.style.borderRadius = "5px";
-          infoDiv.style.boxShadow = "0 2px 4px rgba(255, 255, 255, 0.1)";
-          infoDiv.style.border = "1px solid #ccc";
-          infoDiv.style.width = "480px";
-          infoDiv.style.height = "90px";
-          infoDiv.style.position = "absolute";
-          infoDiv.style.marginLeft = "-1044px";
-          infoDiv.style.color = "#1f2a56";
-          infoDiv.style.textAlign = "left";
-          infoDiv.style.fontSize = "16px";
-          infoDiv.style.fontFamily = "Arial, sans-serif";
-          infoDiv.style.fontWeight = "bold";
-          artNameCont.appendChild(infoDiv);
-        }
-      })
-      .catch(error => {
-        console.error("Error loading info from " + url, error);
-      });
-  });
-  
+ 
